05f8665427bbcf35cfc902f62c993833f475bcf1
jeremyfromearth <jeremynealbrown@gmail.com>
2018-02-27 17:26:18 -0800

    Cleaned up code a bit

--- src/basic.js
+++ src/basic.js
@@ -2,15 +2,19 @@ import Pixelspace from 'lib/pixelspace';
 
 class Basic extends Pixelspace.Renderer {
   init() {
+    // set the background color
     this.bg = 'black'
   }
 
   render() {
+    // text 
+    this.color('#fff');
+    this.font('normal 80pt Terminus');
+    this.text(160, 250, '[PIXELSPACE]', false);
+
+    // shapes
     let cx = this.width * 0.5;
     let cy = this.height * 0.5;
-    this.font('normal 80pt Terminus');
-    this.color('#fff');
-    this.text(160, 250, '[PIXELSPACE]', false);
     this.color('cyan');
     this.rectangle(cx-65, cy, 40, 40);
     this.color('magenta');
