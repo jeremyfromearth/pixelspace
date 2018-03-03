23ffa210eca6e7ddf574af1275ca0232b19be6a7
jeremyfromearth <jeremynealbrown@gmail.com>
2018-03-02 19:31:23 -0800

    Moved demos into the project, with the intent of making them runnable through GH pages

---
+++ demo/js/config.js
@@ -0,0 +1,9 @@
+require({
+  baseUrl:'../../',
+  paths: {
+    js: 'demo/js',
+    text: 'demo/js/text',
+    src: 'demo/src/',
+    lib: 'lib',
+  }
+});
--- js/config.js
+++
@@ -1,9 +0,0 @@
-require({
-  baseUrl:'../../',
-  paths: {
-    js: 'pixelspace-demo/js',
-    text: 'pixelspace-demo/js/text',
-    src: 'pixelspace-demo/src/',
-    lib: 'pixelspace/lib',
-  }
-});
